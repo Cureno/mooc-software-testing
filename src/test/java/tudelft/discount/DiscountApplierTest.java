7dc8ea6e0bb08d844d49dd948f8c5b148e54fe85
Cureno <cureno@users.noreply.github.com>
2019-05-18 21:32:28 +0200

    Topic 4, Practical Exercise #2

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,52 @@
 package tudelft.discount;
 
-public class DiscountApplierTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DiscountApplierTest {
+    @Test
+    void setNewPriceForSingleBusinessProduct() {
+
+        ProductDao mockDao = mock(ProductDao.class);
+
+
+        Product businessProduct = new Product("Rolex", 10.0, "BUSINESS");
+        List<Product> products = Arrays.asList(
+                businessProduct
+        );
+
+        when(mockDao.all()).thenReturn(products);
+        DiscountApplier discountApplier = new DiscountApplier(mockDao);
+
+        discountApplier.setNewPrices();
+
+        Assertions.assertEquals(11.0, businessProduct.getPrice());
+
+    }
+
+    @Test
+    void setNewPriceForSingleHomeProduct() {
+
+        ProductDao mockDao = mock(ProductDao.class);
+
+
+        Product homeProduct = new Product("Lava Lamp", 10.0, "HOME");
+        List<Product> products = Arrays.asList(
+                homeProduct
+        );
+
+        when(mockDao.all()).thenReturn(products);
+        DiscountApplier discountApplier = new DiscountApplier(mockDao);
+
+        discountApplier.setNewPrices();
+
+        Assertions.assertEquals(9.0, homeProduct.getPrice());
+
+    }
 }
