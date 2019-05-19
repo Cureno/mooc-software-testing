c15cfa43349960b72ecb569d532b4dad6ac1c6fa
Cureno <cureno@users.noreply.github.com>
2019-05-19 18:50:03 +0200

    Topic 5, Practical Exercise #1

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,63 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MirrorTest {
+
+    private Mirror mirror;
+
+    @BeforeEach
+    void setUp() {
+        mirror = new Mirror();
+    }
+
+
+    @Test
+    void oddNumberOfCharactersMatch() {
+        assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+        assertEquals("ab", mirror.mirrorEnds("abXYba"));
+    }
+
+    @Test
+    void evenNumberOfCharactersMatch() {
+        assertEquals("a", mirror.mirrorEnds("abca"));
+    }
+
+    @Test
+    void evenNumberOfCharactersMatch_InOddLengthWord() {
+        assertEquals("a", mirror.mirrorEnds("abcda"));
+    }
+    @Test
+    void evenNumberOfCharactersMatch_InThreeCharactersLongWord() {
+        assertEquals("a", mirror.mirrorEnds("aba"));
+    }
+
+    @Test
+    void evenNumberOfCharactersMatch_InFiveCharactersLongWord() {
+        assertEquals("a", mirror.mirrorEnds("abbca"));
+    }
+
+    @Test
+    void threeCharactersMatch() {
+        assertEquals("abc", mirror.mirrorEnds("abccba"));
+    }
+
+    @Test
+    void twoCharactersMatch() {
+        assertEquals("a", mirror.mirrorEnds("aa"));
+    }
+
+    @Test
+    void singleCharacter() {
+        // Is this behavior a bug or a feature?
+        assertEquals("", mirror.mirrorEnds("a"));
+    }
+
+    @Test
+    void emptyString() {
+        assertEquals("", mirror.mirrorEnds(""));
+    }
 }
